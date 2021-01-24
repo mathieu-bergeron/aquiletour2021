9dc8300eb0284d489fe06fee3033407057a443f4
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-24 15:33:48 -0500

    fusion

---
+++ aquiletour_server/src/main/java/ca/aquiletour/server/http/WriteResponseTask.java
@@ -0,0 +1,45 @@
+package ca.aquiletour.server.http;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.eclipse.jetty.server.Request;
+
+import ca.aquiletour.web.HandlerTask;
+import ca.ntro.core.tasks.NtroTask;
+
+public class WriteResponseTask extends NtroTask {
+	
+	private OutputStream out;
+	private Request baseRequest;
+	
+	public WriteResponseTask(Request baseRequest, OutputStream out) {
+		this.baseRequest = baseRequest;
+		this.out = out;
+	}
+
+	@Override
+	protected void runTask() {
+		
+		StringBuilder builder = new StringBuilder();
+		getPreviousTask(HandlerTask.class).writeHtml(builder);
+		
+		try {
+
+			out.write(builder.toString().getBytes());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		baseRequest.setHandled(true);
+		notifyTaskFinished();
+	}
+
+	@Override
+	protected void onFailure(Exception e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
