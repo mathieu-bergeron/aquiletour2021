fcd284a7071746e0f8292bef0b594e1f69bae5c2
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-28 11:01:51 -0500

    ajout des tests

--- aquiletour_server/src/test/java/ca/aquiletour/test/GithubTasksTest.java
+++ aquiletour_server/src/test/java/ca/aquiletour/test/GithubTasksTest.java
@@ -2,13 +2,17 @@ package ca.aquiletour.test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Test;
+import static org.junit.Assert.assertNotEquals;
 
 public class GithubTasksTest {
-	
+
+	@Test
 	public void gitHubFetchTasksTest() {
 		
 		WebDriver driver = new ChromeDriver();
-		System.out.println("Bonjour Selenium");
+
+		assertNotEquals(null, driver);
 	}
 
 }
