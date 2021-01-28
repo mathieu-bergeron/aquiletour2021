df82c9e6fdd8a3280572059382cef29410f55215
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-28 10:30:53 -0500

    fusion

--- aquiletour_javafx/src/test/java/ca/aquiletour/test/GithubTasksTest.java
+++ aquiletour_javafx/src/test/java/ca/aquiletour/test/GithubTasksTest.java
@@ -1,13 +1,14 @@
 package ca.aquiletour.test;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GithubTasksTest {
 	
 	public void gitHubFetchTasksTest() {
 		
 		WebDriver driver = new ChromeDriver();
+		System.out.println("Bonjour Selenium");
 	}
 
 }
