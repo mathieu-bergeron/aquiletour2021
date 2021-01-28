661d6a5e90acf4b8e47c9fecfb6604e026f31dbf
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-28 11:21:35 -0500

    ajout) tests avec Selenium dans aquiletour_server

---
+++ aquiletour_server/src/test/java/ca/aquiletour/test/GithubTasksTest.java
@@ -0,0 +1,30 @@
+package ca.aquiletour.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.Test;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class GithubTasksTest {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		driver = new FirefoxDriver();
+	}
+
+	@Test
+	public void gitHubFetchTasksTest() {
+		assertNotEquals(null, driver);
+	}
+
+	@After
+	public void tearDown() {
+		driver.close();
+	}
+
+}
