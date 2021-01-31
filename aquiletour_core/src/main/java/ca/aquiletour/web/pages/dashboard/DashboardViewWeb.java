65f28dbdb9ccfec79226441e1fddc34f782009d9
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-31 05:28:28 -0500

    en cours

--- aquiletour_core/src/main/java/ca/aquiletour/web/pages/dashboard/DashboardViewWeb.java
+++ aquiletour_core/src/main/java/ca/aquiletour/web/pages/dashboard/DashboardViewWeb.java
@@ -15,8 +15,21 @@ public class DashboardViewWeb extends NtroViewWeb implements DashboardView {
 		HtmlElement container = this.getRootElement().children("#courses-container").get(0);
 		
 		MustNot.beNull(container);
+		
+		StringBuilder html = new StringBuilder();
+		html.append("<div class='card border mb-3' style='max-width: 15rem;'>");
+		html.append("<div class='card-header bg-transparent border'>");
+		html.append(title);
+		html.append("<a id='availableLink' href='#'><span class='color' id='teacherAvailable' ></span></a>");
+		html.append("</div>");
+		html.append("<div class='card-body text'>");
+		html.append("<h5 class='card-title'>TP1</h5>");
+		html.append("<div class='card-footer bg-transparent border'>15 Février 2021</div>");
+		html.append("</div>");
+		html.append("<button type='button' id='buttonAvailable' class='btn btn-primary'>Disponible</button>");
+		html.append("</div>");
 
-		container.appendHtml("<div>" + title + "</div>");
+		container.appendHtml(html.toString());
 	}
 
 }
