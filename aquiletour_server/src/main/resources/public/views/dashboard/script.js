cea95f12791cf955f9543b29016b2644a0b50798
zouz82 <jagal_zouzou@hotmail.com>
2021-02-11 11:04:32 -0500

    Ajustement du footer et fonctionnement du fichier script.js

---
+++ aquiletour_server/src/main/resources/public/views/dashboard/script.js
@@ -0,0 +1,51 @@
+console.log("Charged");
+
+
+
+window.onload = function(){
+  var buttonAvailable = document.getElementById("buttonAvailable");
+  var available = document.getElementById("teacherAvailable");
+  var availableLink = document.getElementById("availableLink");
+  var confirmToast = document.getElementById("btn-confirm");
+  
+  buttonAvailable.onclick = function() {
+    
+    if (available.style.background == "green") {
+      available.style.background = "red";
+      disableLink();
+      
+    } else {
+      available.style.background = "green";
+      showLink();    
+    }
+    
+  }
+}
+
+
+
+$("#btn-confirm").click(function(){
+    $('.toast').toast('show');
+});
+
+document.querySelector('#btn-confirm').addEventListener('click', e => {
+	alert('Un travail a été ajouté!');
+});
+
+function disableLink() {
+
+  availableLink.disabled=true;
+  availableLink.removeAttribute('href');    
+  availableLink.style.textDecoration = 'none';
+  availableLink.style.cursor = 'default';
+}
+
+function showLink() {
+  availableLink.disabled=false;
+  //assign href dynamically
+  availableLink.href = "somepage.html";
+  availableLink.style.textDecoration = "underline";
+  availableLink.style.cursor = "hand";
+}
+
+
