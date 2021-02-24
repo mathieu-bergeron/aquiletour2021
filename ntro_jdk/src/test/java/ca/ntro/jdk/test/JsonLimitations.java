e47020a52766d706a7df9a25ac6a95f65928250e
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-02-24 09:40:34 -0500

    ajout) test pour illustrer le non-support pour les cycles en Json

--- ntro_jdk/src/test/java/ca/ntro/jdk/test/JsonLimitations.java
+++ ntro_jdk/src/test/java/ca/ntro/jdk/test/JsonLimitations.java
@@ -1,6 +1,7 @@
-package ca.ntro.core.test;
+package ca.ntro.jdk.test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -25,21 +27,43 @@ public class JsonLimitations {
 	@Before
 	public void setUp() {
 	}
+	
+	private class LinkedListNode {
+		public LinkedListNode next;
+	}
+
+	@Test
+	public void jsonCycle() throws FileNotFoundException, IOException {
+		
+		LinkedListNode a = new LinkedListNode();
+		LinkedListNode b = new LinkedListNode();
+		
+		a.next = b;
+		b.next = a;
+		
+		// a json object should not have any cycle
+		//
+		// JSON.stringify(a);
+		// Uncaught TypeError: cyclic object value
+	    // <anonymous> debugger eval code:1
+
+		assertThrows(StackOverflowError.class, new ThrowingRunnable() {
+			@Override
+			public void run() throws Throwable {
+				gson.toJson(a);
+			}
+		});
+	}
 
 	private interface ListItem {
-		
 	}
 	
 	private class ListItemA implements ListItem {
-		
 		public int propA = 10;
-		
 	}
 
 	private class ListItemB implements ListItem {
-
 		public int propB = 10;
-
 	}
 
 	@Test
@@ -52,7 +76,7 @@ public class JsonLimitations {
 		assertTrue(list.get(0) instanceof ListItemA);
 		assertTrue(list.get(1) instanceof ListItemB);
 		
-		new FileOutputStream(new File("jsonLimitations.json")).write(gson.toJson(list).getBytes());
+		new FileOutputStream(new File("gsonLimitations.json")).write(gson.toJson(list).getBytes());
 		
 		List<ListItem> parsedList = gson.fromJson(new FileReader(new File("jsonLimitations.json")), List.class);
 		
@@ -61,10 +85,27 @@ public class JsonLimitations {
 		assertFalse(parsedList.get(0) instanceof ListItemA);
 		assertFalse(parsedList.get(1) instanceof ListItemB);
 
-		// XXX: Gson deserializes to LinkedTreeMap and throws ClassCastException
+		// XXX: Gson deserializes list items to LinkedTreeMap 
 		//      when we try to access the value as its origin class
 		assertTrue(((Object) parsedList.get(0)) instanceof LinkedTreeMap);
 		assertTrue(((Object) parsedList.get(1)) instanceof LinkedTreeMap);
+		
+		
+		// XXX: trying to cast the objects will throw
+		//      a ClassCastException
+		assertThrows(ClassCastException.class, new ThrowingRunnable() {
+			@Override
+			public void run() throws Throwable {
+				ListItemA itemA = (ListItemA) parsedList.get(0);
+			}
+		});
+
+		assertThrows(ClassCastException.class, new ThrowingRunnable() {
+			@Override
+			public void run() throws Throwable {
+				ListItemB itemB = (ListItemB) parsedList.get(1);
+			}
+		});
 		
 	}
 }
