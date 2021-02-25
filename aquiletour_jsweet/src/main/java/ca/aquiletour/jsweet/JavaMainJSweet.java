0835805274deb933af2a4d02525b9b97a4f680dd
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-02-25 10:28:48 -0500

    FIXME) toJsonObject récursif dans List et Map fonctionne pas en JSweet

--- aquiletour_jsweet/src/main/java/ca/aquiletour/jsweet/JavaMainJSweet.java
+++ aquiletour_jsweet/src/main/java/ca/aquiletour/jsweet/JavaMainJSweet.java
@@ -17,9 +17,14 @@
 
 package ca.aquiletour.jsweet;
 
+import ca.aquiletour.jsweet.test.LinkedListNode;
+import ca.ntro.core.Ntro;
+import ca.ntro.core.json.JsonObject;
+import ca.ntro.core.json.JsonParser;
 import ca.ntro.core.system.trace.__T;
 import ca.ntro.jsweet.NtroJSweet;
 import ca.ntro.jsweet.introspection.IntrospectorJSweet;
+import ca.ntro.jsweet.services.JsonParserJSweet;
 
 public class JavaMainJSweet {
 
@@ -28,9 +33,34 @@ public class JavaMainJSweet {
 
 		String[] options = new String[] {"--traceLevel","APP"};
 		
-		IntrospectorJSweet introspector = new IntrospectorJSweet();
+		Ntro.__registerIntrospector(new IntrospectorJSweet());
+		JsonParser.initialize(new JsonParserJSweet());
+
+		LinkedListNode a = new LinkedListNode("A");
+		LinkedListNode b = new LinkedListNode("B");
+		LinkedListNode c = new LinkedListNode("C");
+		LinkedListNode d = new LinkedListNode("D");
 		
-		System.out.println("fullName: " + introspector.getFullNameForClass(JavaMainJSweet.class));
+		//a.setNext(a);
+
+		//a.setNext(b);
+		//b.setNext(a);
+		
+		a.setNext(b);
+		b.setNext(c);
+		c.setNext(d);
+		d.setNext(c);
+		
+		c.getNextMap().put("d", d);
+		d.getNextMap().put("b", b);
+		
+		b.getNextList().add(b);
+		b.getNextList().add(c);
+		b.getNextList().add(d);
+		
+		JsonObject jsonObject = a.toJsonObject();
+		
+		System.out.println(JsonParser.toString(jsonObject));
 		
 
 		NtroJSweet.defaultInitializationTask()
