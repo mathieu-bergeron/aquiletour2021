253a5263ed7bc9d3023f7fab2c0ae0c564871cb5
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-02-25 09:42:32 -0500

    en cours

--- ntro_core/src/main/java/ca/ntro/core/json/JsonObjectIO.java
+++ ntro_core/src/main/java/ca/ntro/core/json/JsonObjectIO.java
@@ -44,6 +44,14 @@ public abstract class JsonObjectIO implements Serializable {
 	public JsonObject toJsonObject() {
 		T.call(this);
 		
+		Map<Object, String> serializedObjects = new HashMap<>();
+		
+		return toJsonObject(serializedObjects);
+	}
+
+	private JsonObject toJsonObject(Map<Object, String> serializedObjects) {
+		T.call(this);
+		
 		JsonObject jsonObject = JsonParser.jsonObject();
 
 		jsonObject.setTypeName(Ntro.introspector().getSimpleNameForClass(this.getClass()));
@@ -66,7 +74,7 @@ public abstract class JsonObjectIO implements Serializable {
 			
 			// TODO: go inside a Map or a List to look
 			//       for appointment-defined values (JsonObjectIO)
-			Object jsonValue = buildJsonValue(value);
+			Object jsonValue = buildJsonValue(serializedObjects, value);
 			
 			jsonObject.put(fieldName, jsonValue);
 		}
@@ -75,7 +83,7 @@ public abstract class JsonObjectIO implements Serializable {
 	}
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private Object buildJsonValue(Object value) {
+	private Object buildJsonValue(Map<Object, String> serializedObjects, Object value) {
 		
 		Object jsonValue = value;
 
@@ -95,7 +103,7 @@ public abstract class JsonObjectIO implements Serializable {
 				
 				Object mapValue = map.get(key);
 				
-				Object jsonMapValue = buildJsonValue(mapValue);
+				Object jsonMapValue = buildJsonValue(serializedObjects, mapValue);
 				
 				result.put(key, jsonMapValue);
 			}
@@ -110,7 +118,7 @@ public abstract class JsonObjectIO implements Serializable {
 			
 			for(Object item : list) {
 				
-				Object jsonItem = buildJsonValue(item);
+				Object jsonItem = buildJsonValue(serializedObjects, item);
 				
 				result.add(jsonItem);
 			}
