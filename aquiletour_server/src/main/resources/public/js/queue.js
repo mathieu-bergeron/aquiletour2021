0f1cbfd1f14ab3d58a7f7ba3327ef6b8afb7e6ca
zouz82 <jagal_zouzou@hotmail.com>
2021-02-26 13:49:39 -0500

    Ajout de la requête lorsqu'un apoointment change de place dans la queue

--- aquiletour_server/src/main/resources/public/js/queue.js
+++ aquiletour_server/src/main/resources/public/js/queue.js
@@ -15,7 +15,28 @@ window.onload = function(){
         $( "#appointment-list" ).sortable({
             handle:'.handle',
             update: function(event, ui){
-                window.location = "/moveId=&beforeId=";
+                
+
+                const appointment = $(ui.item);
+                const nextAppointment = $(appointment.next());
+                const prevAppointment = $(appointment.prev());
+
+                const appointmentId = appointment.attr("id");
+                const nextAppointmentId = nextAppointment.attr("id");
+                const prevAppointmentId = prevAppointment.attr("id");
+
+                
+
+                if (nextAppointment.length > 0) {
+                    
+                    window.location = window.location.pathname + "?moveId=" + appointmentId + "&beforeId=" + nextAppointmentId;
+                    
+                } else if (prevAppointment.length > 0){
+                 
+                    window.location = window.location.pathname + "?moveId=" + appointmentId + "&afterId=" + prevAppointmentId;
+                }
+
+                console.log(nextAppointment);
             }
            
         });
