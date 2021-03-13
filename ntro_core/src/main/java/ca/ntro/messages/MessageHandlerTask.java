313959b6a162af096756508c803ff04a929fd262
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-03-13 06:58:58 -0500

    fusion

--- ntro_core/src/main/java/ca/ntro/messages/MessageHandlerTask.java
+++ ntro_core/src/main/java/ca/ntro/messages/MessageHandlerTask.java
@@ -22,24 +22,29 @@ public class MessageHandlerTask<MSG extends NtroMessage> extends NtroTaskAsync {
 	}
 
 	public void triggerHandlerOnce() {
-		
-		// FIXME: as implemented below
-		//        there is an issue with two message
-		//        handlers one after the other
-		notifyTaskFinished();  // unblock the handler
-		execute();             // execute tasks that are now unblocked
-		resetNodeTransitive(); // reblock tasks that depend on this handler
-		execute();             // get ready for next trigger
+		/* FIXME: as implemented below
+		 * I think there is an issue with two message
+		 * handlers one after the other
+		 * 
+		 * if A --> B --> C
+		 * 
+		 * if B is ready before A
+		 * do we incorrectly reset it and
+		 * loose the fact that it was ready??
+		 * 
+		 */
 
-	public void triggerHandlerOnce() {
-		// FIXME: as implemented below
-		//        there is an issue with two message
-		//        handlers one after the other
 		notifyTaskFinished();  // unblock the handler
 		execute();             // execute tasks that are now unbloc 
 		                       // reblock tasks that depend on this handlerked
 		resetNodeTransitive(TaskState.WAITING_FOR_PREVIOUS_TASKS);
 		execute();             // get ready for next trigger
 
+	}
+
+	@Override
+	protected void onFailure(Exception e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
