5c7776c60fe547a81fa948224da240254fa5a6a3
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-03-17 13:11:48 -0400

    en cours

--- aquiletour_server/src/main/java/ca/aquiletour/server/email/TestEmail.java
+++ aquiletour_server/src/main/java/ca/aquiletour/server/email/TestEmail.java
@@ -14,44 +14,46 @@ import javax.mail.internet.MimeMessage;
 // from : https://mkyong.com/java/javamail-api-sending-email-via-gmail-smtp-example/
 public class TestEmail {
 
-	   public static void main(String [] args) {    
-		   
-	        final String username = "aiguilleur.ca@gmail.com";
-	        final String password = "Momo!1234";
+	public static void main(String[] args) {
+		sendCode("143 567", "Mathieu", "mathieu.bergeron@cmontmorency.qc.ca");
+	}
 
-	        Properties prop = new Properties();
-	        prop.put("mail.smtp.host", "smtp.gmail.com");
-	        prop.put("mail.smtp.port", "587");
-	        prop.put("mail.smtp.auth", "true");
-	        prop.put("mail.smtp.starttls.enable", "true"); //TLS
-	        
-	        Session session = Session.getInstance(prop,
-	                new javax.mail.Authenticator() {
-	                    protected PasswordAuthentication getPasswordAuthentication() {
-	                        return new PasswordAuthentication(username, password);
-	                    }
-	                });
+	public static void sendCode(String loginCode, String userName, String toEmail) {
+		final String username = "aiguilleur.ca@gmail.com";
+		final String password = "Momo!1234";
 
-	        try {
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "587");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.starttls.enable", "true"); //TLS
+		
+		Session session = Session.getInstance(prop,
+		        new javax.mail.Authenticator() {
+		            protected PasswordAuthentication getPasswordAuthentication() {
+		                return new PasswordAuthentication(username, password);
+		            }
+		        });
 
-	            Message message = new MimeMessage(session);
-	            message.setFrom(new InternetAddress("aiguilleur.ca@gmail.com"));
-	            message.setRecipients(
-	                    Message.RecipientType.TO,
-	                    InternetAddress.parse("eborgner@gmail.com, mathieu.bergeron@cmontmorency.qc.ca")
-	            );
-	            message.setSubject("Nouveau code: 143 645");
-	            message.setText("Bonjour,"
-	                    + "\n\nVoici votre nouveau code de connexion"
-	                    + "\n\n143 645"
-	                    + "\n\nMerci et bonne journée!\nL'équipe Aiguilleur.ca");
+		try {
 
-	            Transport.send(message);
+		    Message message = new MimeMessage(session);
+		    message.setFrom(new InternetAddress("aiguilleur.ca@gmail.com"));
+		    message.setRecipients(
+		            Message.RecipientType.TO,
+		            InternetAddress.parse(toEmail));
+		    message.setSubject("Nouveau code: " + loginCode);
+		    message.setText("Bonjour " + userName + ","
+		            + "\n\nVoici votre nouveau code de connexion"
+		            + "\n\n" + loginCode
+		            + "\n\nMerci et bonne journée!\nL'équipe Aiguilleur.ca\n\n");
 
-	            System.out.println("Done");
+		    Transport.send(message);
 
-	        } catch (MessagingException e) {
-	            e.printStackTrace();
-	        }
-	    }
+		    System.out.println("Done");
+
+		} catch (MessagingException e) {
+		    e.printStackTrace();
+		}
+	}
 }
