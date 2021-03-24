e1f2da6129421fed2cdea7bf6c8ea9a94f150643
zouz82 <jagal_zouzou@hotmail.com>
2021-03-24 10:07:54 -0400

    Ajout du progress bar et d'un brouillon du bread crumb

--- aquiletour_server/src/main/resources/public/js/dashboard.js
+++ aquiletour_server/src/main/resources/public/js/dashboard.js
@@ -3,15 +3,92 @@ window.onload = function(){
     var buttonAvailable = document.getElementById("buttonAvailable");
     var available = document.getElementById("teacherAvailable");
     var availableLink = document.getElementById("availableLink");
+    var buttonAddCourse = document.getElementById("add-course-submit-button");
+
+    $(function() {
+      
+            
+        $( "#courses-container" ).sortable({
+            handle:'.handle',
+            update: function(event, ui){
+                
+
+                const course = $(ui.item);
+                const nextCourse = $(course.next());
+                const prevCourse = $(course.prev());
+
+                const courseId = course.attr("id");
+                const nextCourseId = nextCourse.attr("id");
+                const prevCourseId = prevCourse.attr("id");
+
+
+                if (nextCourse.length > 0) {
+                    
+                    //window.location = window.location.pathname + "?moveId=" + courseId + "&beforeId=" + nextcourseId;
+                    history.pushState({
+                        id: 'queue'
+                      }, 'Queue', window.location.pathname + "?moveId=" + courseId + "&beforeId=" + nextCourseId);
+                    
+                } else if (prevCourse.length > 0) {
+                
+                    //window.location = window.location.pathname + "?moveId=" + courseId + "&afterId=" + prevcourseId;
+
+                    history.pushState({
+                        id: 'queue'
+                      }, 'Queue', window.location.pathname + "?moveId=" + courseId + "&afterId=" + prevCourseId);
+                }
+
+                console.log(nextCourse);
+            }
+        
+        });
+        
+        
+    });
+    
+    $(document).ready(function() {
+  
+      // get box count
+      var count = 0;
+      var checked = 0;
+      function countBoxes() { 
+        count = $("input[type='checkbox']").length;
+        console.log(count);
+      }
+      
+      countBoxes();
+      $(":checkbox").click(countBoxes);
+      
+      // count checks
+      
+      function countChecked() {
+        checked = $("input:checked").length;
+        
+        var percentage = parseInt(((checked / count) * 100),10);
+        $(".progressbar-bar").progressbar({
+                value: percentage
+            });
+        $(".progressbar-label").text(percentage + "%");
+      }
+      
+      countChecked();
+      $(":checkbox").click(countChecked);
+    });
 
     buttonAvailable.onclick = function() {
       
+      
+
       if (available.style.background == "green") {
         available.style.background = "red";
+        
+        
+
         disableLink();
         
       } else {
         available.style.background = "green";
+        
         showLink();    
       }
       
@@ -33,11 +110,16 @@ window.onload = function(){
       availableLink.style.cursor = "hand";
     }
 
-    $(function() {
-      $( "#course-cards" ).sortable({
-          handle:'.handle'
-      });
-    
-      
-    } );
+    buttonAddCourse.onclick = function() {
+
+      document.getElementById("card-header").className = "ghost-header";
+        document.getElementById("card-body").className = "ghost-body";
+        document.getElementById("shimmering").className = "shimmering";
+
+        setTimeout(function() {
+          document.getElementById("card-header").className = "card-header";
+          document.getElementById("card-body").className = "card-body";
+          document.getElementById("shimmering").className = "";
+        },2500);
+    }    
 }
