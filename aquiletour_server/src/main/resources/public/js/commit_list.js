d356c301931d9f09a6367aaeadc8f1f719fa9c59
marbawws <clibeau2010@hotmail.com>
2021-03-25 17:17:22 -0400

    changement de graph pour open source

--- aquiletour_server/src/main/resources/public/js/commit_list.js
+++ aquiletour_server/src/main/resources/public/js/commit_list.js
@@ -2,59 +2,90 @@ function initializeCommitList(viewRootElement, jSweet) {
 	console.log("initializeCommitList");
 
 	const commitList = viewRootElement.find("#commit-list li");
-
-	window.onload = function() {
-		var chart = new CanvasJS.Chart("chartContainer", {
-			title : {
-				text : window.location.pathname + "'s commit history"
-			},
-			data : [ {
-				type : "column",
-				dataPoints : [
-
-				]
-			} ]
-		});
-		chart.render();
-
+	var ctx = viewRootElement.find("#myChart");
+	var myChart = new Chart(ctx, {
+	    type: 'scatter',
+	    data: {
+	    	datasets: [{
+	    		backgroundColor: 'rgb(255, 30, 30)',
+	            label: "student's commit history",
+	            data: [{
+	            }],
+	         }]
+	    },
+	    options: {
+	    	scales: {
+	    	      xAxes: [{
+	    	        display: true,
+	    	        gridLines: {
+	    	          display: true,
+	    	          color: "#000000"
+	    	        },
+	    	        scaleLabel: {
+	    	          display: true,
+	    	          labelString: 'Timestamp'
+	    	        }
+	    	      }],
+	    	      yAxes: [{
+	    	        display: true,
+	    	        gridLines: {
+	    	          display: true,
+	    	          color: "#000000"
+	    	        },
+	    	        scaleLabel: {
+	    	          display: true,
+	    	          labelString: 'Estimated Effort'
+	    	        }
+	    	      }]
+	    	    },
+	    	tooltips:{
+	    		 callbacks: {
+	                 label: function(tooltipItem, data) {
+	                     var label;
+	                  
+	                     commitList.each(function(i, li) {
+	                    		var commit = $(li)
+		             			var div = commit.children()[0];
+	                    		var timeStampSpan = $(div).find('#timestamp');
+	                    		var timeStamp = parseInt($(timeStampSpan).text());
+	                    		
+	                    		if(timeStamp==tooltipItem.xLabel){
+	                    			var commitMessageSpan = $(div).find('#commitMessage');
+			             			var exercicePathSpan = $(div).find('#exercicePath');
+			             			var modifiedFilesSpan = $(div).find('#modifiedFiles');
+			             			
+			             			var commitMessage = $(commitMessageSpan).text();
+			             			var exercicePath = $(exercicePathSpan).text();
+			             			var modifiedFiles = $(modifiedFilesSpan).text();
+			             			
+			             			label = ["Timestamp : " + tooltipItem.xLabel];
+	         						label.push("Estimated Effort : " + tooltipItem.yLabel);
+	         						label.push("Commit Message : " + commitMessage);
+	         						label.push("Exercice Path : " + exercicePath);
+	         						label.push("Modified Files : " + modifiedFiles); 
+	                    		}                 	             			
+	             		});
+	                     return label;
+	                 }
+	             }
+	    	}
+	    	
+	    }
+	});
+	
 		commitList.each(function(i, li) {
 			var commit = $(li)
 			var div = commit.children()[0];
 			
 			var timeStampSpan = $(div).find('#timestamp');
 			var estimatedEffortSpan = $(div).find('#estimatedEffort');
-			var commitMessageSpan = $(div).find('#commitMessage');
-			var exercicePathSpan = $(div).find('#exercicePath');
-			var modifiedFilesSpan = $(div).find('#modifiedFiles');
 			
 			var timeStamp = parseInt($(timeStampSpan).text());
 			var estimatedEffort = parseInt($(estimatedEffortSpan).text());
-			var commitMessage = $(commitMessageSpan).text();
-			var exercicePath = $(exercicePathSpan).text();
-			var modifiedFiles = $(modifiedFilesSpan).text();
-
-			chart.options.data[0].dataPoints.push({
-				x : timeStamp ,
-				y : estimatedEffort,
-				toolTipContent: "Timestamp : {x}<br/>" +
-						"Estimated Effort : {y}<br/>" +
-						"Commit Message : " + commitMessage + "<br/>" +
-						"Exercice Path : " + exercicePath + "<br/>" +
-						"Modified Files : " + modifiedFiles 
-						
+			
+			myChart.data.datasets.forEach((dataset) => {
+		        dataset.data.push({x:timeStamp,y:estimatedEffort});
 			});
-			chart.render();
+			myChart.update();
 		});
-
-		function toogleDataSeries(e) {
-			if (typeof (e.dataSeries.visible) === "undefined"
-					|| e.dataSeries.visible) {
-				e.dataSeries.visible = false;
-			} else {
-				e.dataSeries.visible = true;
-			}
-			e.chart.render();
-		}
-
-	}
 }
