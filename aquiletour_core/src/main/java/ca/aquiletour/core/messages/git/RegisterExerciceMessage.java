b8f1f0f4059618865c8af8a4b3f8f6c7c3709925
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-03-29 05:47:44 -0400

    ajout) CommitListModel dans les __test_data__

--- aquiletour_core/src/main/java/ca/aquiletour/core/messages/git/RegisterExerciceMessage.java
+++ aquiletour_core/src/main/java/ca/aquiletour/core/messages/git/RegisterExerciceMessage.java
@@ -6,32 +6,39 @@ public class RegisterExerciceMessage extends NtroMessage {
 	
 	private String courseId;
 	private String exercicePath;
-	private String sourceDirectory;
+	private String directoryName;
 	private String completionKeywords;
 	
 	public String getCourseId() {
 		return courseId;
 	}
+
 	public void setCourseId(String courseId) {
 		this.courseId = courseId;
 	}
+
 	public String getExercicePath() {
 		return exercicePath;
 	}
+
 	public void setExercicePath(String exercicePath) {
 		this.exercicePath = exercicePath;
 	}
-	public String getSourceDirectory() {
-		return sourceDirectory;
-	}
-	public void setSourceDirectory(String sourceDirectory) {
-		this.sourceDirectory = sourceDirectory;
-	}
+	
 	public String getCompletionKeywords() {
 		return completionKeywords;
 	}
+
 	public void setCompletionKeywords(String completionKeywords) {
 		this.completionKeywords = completionKeywords;
+	}
+
+	public String getDirectoryName() {
+		return directoryName;
+	}
+
+	public void setDirectoryName(String directoryName) {
+		this.directoryName = directoryName;
 	}
 
 }
