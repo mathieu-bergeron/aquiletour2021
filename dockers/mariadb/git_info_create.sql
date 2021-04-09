9a4eedbb6d8f52402ebca94ad7040b36dea2979d
Nicolas Leduc <hexabit@gmail.com>
2021-04-09 10:23:48 -0400

    Mise a jour de la DB git_info afin de repondre aux requetes de l'api GIT
    TODO: ajout de trigger afin de valider certaines informations

--- dockers/mariadb/git_info_create.sql
+++ dockers/mariadb/git_info_create.sql
@@ -1,45 +1,78 @@
 CREATE DATABASE IF NOT EXISTS git_info;
 USE git_info;
 
-# Table DEPOTS:
-# (URL Depot) - Session - Cours - Groupe - Etudiant - TP
-CREATE TABLE IF NOT EXISTS depot (
-	url_depot VARCHAR(255) PRIMARY KEY NOT NULL,
-	depot_host CHAR(2) NOT NULL CHECK (depot_host RLIKE '^[A-Z][A-Z]$'),
-	session CHAR(3) NOT NULL CHECK (session RLIKE '^[A-Z][0-9][0-9]$'),
-	cours CHAR(7) NOT NULL CHECK (cours RLIKE '^[0-9][0-9][0-9]-[A-Z0-9][A-Z0-9][A-Z0-9]$'),
-	groupe CHAR(2) NOT NULL CHECK (groupe RLIKE '^[0-9][0-9]$'),
-	etudiant_id CHAR(7) NOT NULL CHECK (etudiant_id RLIKE '^[0-9][0-9][0-9][0-9][0-9][0-9][0-9]$'),
-	section VARCHAR(255));
+# Session : "H2021"
+# Cours : "mathieu.bergeron/StruDon"
+# Groupe : "01"
+# CalPath : "/TP1/Exercice 1"
+# DepotPath : "/TP1"
+# FilePath : "/exercice01"
+# CompletionKW : "Exercice 1"
+# URLDepot : "https://github.com/test/test.git"
+# Etudiant : "bob.berancourt"
+# CommitID : "asdfasdfw34qfadsfasdrf3"
+# Date : "1615215942"
+# ShortMessage : "Blah Blah"
+# Message : "Blah Blah Blah Blah"
+# FilePath : "/src/atelier2/Exercise3.java"
+# Insert : "5"
+# Delete : "10"
+# Effort : "34"
+
+# Table EXERCICE:
+# (Session - Cours - Groupe - CalPath) - DepotPath - FilePath - CompletionKW
+CREATE TABLE IF NOT EXISTS exercise (
+	session_id CHAR(5) NOT NULL CHECK (session_id RLIKE '^[A-Z][0-9][0-9][0-9][0-9]$'),
+	course_id VARCHAR(127) NOT NULL CHECK (course_id RLIKE '^[a-z0-9.-]+/[a-z0-9_.-]+$'),
+	group_id CHAR(2) NOT NULL CHECK (group_id RLIKE '^[0-9][0-9]$'),
+	exercise_path VARCHAR(255) NOT NULL,
+	repo_path VARCHAR(255) NOT NULL,
+	file_path VARCHAR(255) NOT NULL,
+	completion_kw VARCHAR(127),
+	PRIMARY KEY (session_id, course_id, group_id, exercise_path));
+
+# Table DEPOT:
+# (URL Depot) - Session - Cours - Groupe - Etudiant - DepotPath
+CREATE TABLE IF NOT EXISTS repository (
+	repo_url VARCHAR(255) PRIMARY KEY NOT NULL,
+	repo_host CHAR(2) NOT NULL CHECK (repo_host RLIKE '^[A-Z][A-Z]$'),
+	session_id CHAR(5) NOT NULL CHECK (session_id RLIKE '^[A-Z][0-9][0-9][0-9][0-9]$'),
+	course_id VARCHAR(127) NOT NULL CHECK (course_id RLIKE '^[a-z0-9.-]+/[a-z0-9_.-]+$'),
+	group_id CHAR(2) NOT NULL CHECK (group_id RLIKE '^[0-9][0-9]$'),
+	student VARCHAR(127) NOT NULL CHECK (student RLIKE '^[a-z0-9.-]+$'),
+	repo_path VARCHAR(255) NOT NULL
+#	FOREIGN KEY (session_id, course_id, group_id, repo_path) REFERENCES exercise(session_id, course_id, group_id, repo_path)
+# 	TODO: Trigger to validate data
+	);
 
 # Table COMMIT:
-# (URL Depot - Commit ID) - Date - Message
+# (URL Depot - Commit ID) - Date - ShortMessage - Message - CalPath
 CREATE TABLE IF NOT EXISTS commit (
-	url_depot VARCHAR(255) NOT NULL,
-	commit_id CHAR(40) NOT NULL,
+	repo_url VARCHAR(255) NOT NULL,
+	commit_id CHAR(40) NOT NULL CHECK (commit_id RLIKE '^[a-f0-9]{40}$'),
 	commit_date DATETIME NOT NULL,
+	summary VARCHAR(255),
 	message TEXT,
-	PRIMARY KEY (url_depot, commit_id),
-	FOREIGN KEY (url_depot) REFERENCES depot(url_depot));
+	exercise_path VARCHAR(255),
+	PRIMARY KEY (repo_url, commit_id),
+	FOREIGN KEY (repo_url) REFERENCES repository(repo_url)
+#	FOREIGN KEY (exercise_path) REFERENCES exercise(exercise_path)
+# 	TODO: Trigger to validate data
+	);
 
 # Table FICHIER:
-# (URL Depot - Commit ID - Fichier Path) - Insert - Delete
-CREATE TABLE IF NOT EXISTS element (
-	url_depot VARCHAR(255) NOT NULL,
-	commit_id CHAR(40) NOT NULL,
+# (URL Depot - Commit ID - FilePath) - Insert - Delete - Effort - CalPath
+CREATE TABLE IF NOT EXISTS commit_file (
+	repo_url VARCHAR(255) NOT NULL,
+	commit_id CHAR(40) NOT NULL CHECK (commit_id RLIKE '^[a-f0-9]{40}$'),
 	file_path VARCHAR(255) NOT NULL,
-	insert_count INT,
-	delete_count INT,
-	PRIMARY KEY (url_depot, commit_id, file_path),
-	FOREIGN KEY (url_depot, commit_id) REFERENCES commit(url_depot, commit_id));
+	insert_count INT NOT NULL,
+	delete_count INT NOT NULL,
+	effort INT NOT NULL,
+	exercise_path VARCHAR(255) NOT NULL,
+	PRIMARY KEY (repo_url, commit_id, file_path),
+	FOREIGN KEY (repo_url, commit_id) REFERENCES commit(repo_url, commit_id)
+#	FOREIGN KEY (exercise_path) REFERENCES exercise(exercise_path)
+# 	TODO: Trigger to validate data
+	);
 
-# Table TAGS:
-# (URL Depot - Commit ID - Tag) - File(O/N) - Message(O/N)
-CREATE TABLE IF NOT EXISTS category (
-	url_depot VARCHAR(255) NOT NULL,
-	commit_id CHAR(40) NOT NULL,
-	category VARCHAR(255) NOT NULL,
-	from_file BOOLEAN,
-	from_message BOOLEAN,
-	PRIMARY KEY (url_depot, commit_id, category),
-	FOREIGN KEY (url_depot, commit_id) REFERENCES commit(url_depot, commit_id));
