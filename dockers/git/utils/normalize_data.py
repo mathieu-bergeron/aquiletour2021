6de28480b7489522230d72d424fea5436b38dda9
Nicolas Leduc <hexabit@gmail.com>
2021-04-20 10:23:45 -0400

    First version of the patch matching algorithm... Needs cleanup.

--- dockers/git/utils/normalize_data.py
+++ dockers/git/utils/normalize_data.py
@@ -1,4 +1,6 @@
 import re
+import os
+import Levenshtein as le
 
 # lettre + 4 chiffres -> 'H2021'
 # Accept: hiver 2021, hiver-2021, hiver2021, hiver21, h2021, h21
@@ -43,30 +45,104 @@ def normalize_studentId(student_arg):
 
 def clean_path(path):
     # Clean path (remove space,-,_)
-    # Lowercase path
+    cp = re.sub('[ _-]', '', path)
+    cp = cp.lower().replace('\\','/')
     # Split path by word and number
-    # Create 2-tuple (word, number)  number may be None if 2 consecutive words
-    pass
+    dir_list = re.split('[/.]', cp)
+    dir_part = None
+    tuple_list = []
+    for one_dir in dir_list:
+        match = re.match('([a-z]*)([0-9]*)([a-z0-9]*)',one_dir)
+        # Create 2-tuple (word, number)  number may be None if 2 consecutive words
+        if match.group(1) != '':
+            if dir_part:
+                tuple_list.append((dir_part, None))
+            dir_part = match.group(1)
+        if match.group(2) != '':
+            number_part = int(match.group(2))
+            tuple_list.append((dir_part, str(number_part) + match.group(3)))
+            dir_part = None
+    return tuple_list
 
 def match_tuples(tuple1, tuple2):
-    # Levenstein compare the two words (1st element)
+    match = False
+    edits = le.editops(tuple1[0], tuple2[0])
+    edits_counts = {}
+    for edit,_,_ in edits:
+        edits_counts[edit] = edits_counts.get(edit, 0) + 1
+    inserts = edits_counts.get('insert',0) + edits_counts.get('replace',0)
+    deletes = edits_counts.get('delete',0) + edits_counts.get('replace',0)
     # if only insert and max 1 delete or replace
-    # OR if only delete with max 1 insert ore replace
-    #   Compare number (2nd element)
-    #   if same then tuple match
-    # tuples doesn't match
-    pass
+    # OR if only delete with max 1 insert or replace
+    if (inserts <= 1 or deletes <= 1) and tuple1[1] == tuple2[1]: 
+        match = True
+    return match
 
-def path_match(exercicePath, depotPath, filePath):
-    
-    # Match and remove depotPath from exercicePath
-    # Clean exercicePath and filePath
-    # filePath tuple = 0
+def match_path(file_path_ex, file_path_commit):
+    # Clean exercise and commit file path
+    tuples_exercise = clean_path(file_path_ex)
+    tuples_commit = clean_path(file_path_commit)
+    nb_match = 0
+    # TODO: Make sure that the matching tuples are in the same order in the two paths
+    for ex_tuple in tuples_exercise:
+        match = False
+        for c_tuple in tuples_commit:
+            match |= match_tuples(ex_tuple, c_tuple)
+        if match:
+            nb_match += 1
+    return nb_match, len(tuples_exercise)
+    # max(len(tuples_exercise), len(tuples_commit))
     # For each tuple in exercicePath tuples
     #   find the matching tuple from the last match in filePath tuples
     #   if no more tuple in filePath then path doesn't match
-    # if all exercicePath tuple match then path match
     # return the match level (# of tuples matched) : max match is the exercice path for the file
+
+def find_exercise(exercicePath_list, repo_path_list_ex, file_path_list_ex, repo_path_commit, file_path_commit):
+    matched_exPath_list = []
+    max_match_value = 0
+    min_match_len = 99
+    if repo_path_commit in repo_path_list_ex:
+        for exPath, repoPath, filePath in zip(exercicePath_list,repo_path_list_ex, file_path_list_ex):
+            if repo_path_commit == repoPath:
+                nb_match, match_len = match_path(filePath, file_path_commit)
+                if nb_match > max_match_value:
+                    max_match_value = nb_match
+                    min_match_len = match_len
+                    matched_exPath_list = [exPath]
+                elif nb_match == max_match_value:
+                    if match_len < min_match_len:
+                        min_match_len = match_len
+                        matched_exPath_list = [exPath]
+                    elif match_len == min_match_len:
+                        matched_exPath_list.append(exPath)
+    else:
+        for exPath, repoPath, filePath in zip(exercicePath_list,repo_path_list_ex, file_path_list_ex):
+            if repoPath.startswith(repo_path_commit):
+                filePath = str(os.path.join(repoPath.split(repo_path_commit,1)[1], filePath))
+            elif repo_path_commit.startswith(repoPath):
+                file_path_commit = str(os.path.join(repo_path_commit.split(repoPath,1)[1], file_path_commit) )
+            else:
+                filePath = None
+            if filePath:
+                nb_match, match_len = match_path(filePath, file_path_commit)
+                if nb_match > max_match_value:
+                    max_match_value = nb_match
+                    min_match_len = match_len
+                    matched_exPath_list = [exPath]
+                elif nb_match == max_match_value:
+                    if match_len < min_match_len:
+                        min_match_len = match_len
+                        matched_exPath_list = [exPath]
+                    elif match_len == min_match_len:
+                        matched_exPath_list.append(exPath)
+
+    print(matched_exPath_list)
+    # If commit repo in repo_list_ex
+    #   Get the exercice path that get the max match value between file paths
+    # TODO: else Reparer le commit path avec le depot pour essayer de matcher
+    #   Find exercices so repo_commit + file_commit contains all element of repo_ex
+    #   Remove repo_ex from file path
+    #   Get the exercice path that get the max match value between cleaned file paths
     pass
 
 # Some Unit Test
@@ -115,8 +191,87 @@ def test_student():
     print(normalize_studentId('nicolas.leduc') + ' = nicolas.leduc')
     print(normalize_studentId('Pierre-Paul.Bois-Page') + ' = pierre-paul.bois-page')
 
+def test_exercisePath():
+    ex_path_list = ['/',
+                    '/Semaine 1',
+                    '/Semaine 1/TP 1/Exercice 1',
+                    '/Semaine 1/TP 1/Exercice 2',
+                    '/Semaine 2/TravailPratique02',
+                    '/Semaine 2/TravailPratique02/Ex1',
+                    '/Semaine 2/TravailPratique02/Ex2',
+                    '/Semaine 2/TravailPratique02/Ex3',
+                    '/Semaine 3/TP 2/Etape 1',
+                    '/Semaine 3/TP 2/Etape 2',
+                    '/Semaine 3/TP 2/Etape 2/Exercice 1',
+                    '/Semaine 3/TP 2/Etape 3/Exercice 1',
+                    '/Semaine 4',
+                    '/Semaine 4/Travail 4',
+                    '/Semaine 4/Travail 5',
+                    '/Semaine 4/Travail 5/Partie01',
+                    '/Semaine 4/Travail 5/Partie02',
+                    '/Semaine 4/Travail 5/Partie03',
+                    '/Semaine 5',
+                    '/Semaine 5/TP2/Etape 4',
+                    '/Semaine 5/TP2/Etape 5',
+                    '/Semaine 5/TP2/Etape 5/Exercice 1',
+                    '/Semaine 5/TP2/Etape 6',
+                    ]
+    ex_repo_list = ['/',
+                    '/',
+                    '/',
+                    '/',
+                    '/',
+                    '/',
+                    '/',
+                    '/',
+                    '/Semaine 3/TP 2',
+                    '/Semaine 3/TP 2',
+                    '/Semaine 3/TP 2',
+                    '/Semaine 3/TP 2',
+                    '/Semaine 4',
+                    '/Semaine 4',
+                    '/Semaine 4/Travail 5',
+                    '/Semaine 4/Travail 5',
+                    '/Semaine 4/Travail 5',
+                    '/Semaine 4/Travail 5',
+                    '/',
+                    '/',
+                    '/',
+                    '/',
+                    '/',
+                    ]
+    ex_dir_list = ['/',
+                    '/Semaine 1',
+                    '/Semaine 1/TP 1/Exercice 1',
+                    '/Semaine 1/TP 1/Exercice 2',
+                    '/Sem2/TP02',
+                    '/Sem2/TP02/Exercice01',
+                    '/Sem2/TP02/Exercice02',
+                    '/Sem2/TP02/Exercice03',
+                    '/src/Etape1',
+                    '/src/Etape2',
+                    '/src/Etape2/Ex1',
+                    '/src/Etape3/Ex1',
+                    '/',
+                    '/Travail 4',
+                    '/',
+                    '/Partie01',
+                    '/Partie02',
+                    '/Partie03',
+                    '/Semaine 5',
+                    '/Semaine 5/TP02/Etape 4',
+                    '/Semaine 5/TP02a/Etape 5',
+                    '/Semaine 5/TP02/Etape 5/Exercice 1',
+                    '/Semaine 5/TP02/Etape 6',
+                    ]
+    find_exercise(ex_path_list, ex_repo_list, ex_dir_list, '/', '/Sem2/TP02/main.java')
+    find_exercise(ex_path_list, ex_repo_list, ex_dir_list, '/Semaine 3/TP 2', '/src/Etape2/main.java')
+    find_exercise(ex_path_list, ex_repo_list, ex_dir_list, '/Semaine 3', '/src/TravailPratique02/Etape2/main.java')
+    pass
+
 if __name__=="__main__":
     test_session()
     test_course()
     test_group()
     test_student()
+    test_exercisePath()
