faf101ff9efbb7f4a6294199be290e3064408e8f
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-05-04 13:58:36 -0400

    en cours) finalisation de la connexion

--- aquiletour_server/src/main/resources/public/js/root.js
+++ aquiletour_server/src/main/resources/public/js/root.js
@@ -1,19 +1,35 @@
-window.onload = function(){
-    $('.navbar-nav>li>a').on('click', function(){
+function initializeRoot(viewRootElement, jSweet){
+
+    const navBarA = viewRootElement.find('.navbar-nav>li>a');
+    const loginDropdown = viewRootElement.find('#login-dropdown');
+
+    const loginTabStudent = viewRootElement.find('#login-tab-student');
+    const loginTabTeacher = viewRootElement.find('#login-tab-teacher');
+
+    const studentLoginCollapse = viewRootElement.find('#student-login-collapse');
+    const teacherLoginCollapse = viewRootElement.find('#teacher-login-collapse');
+
+    navBarA.on('click', function(){
         $('.navbar-collapse').collapse('hide');
     });
 
-    $('#login-dropdown').on('click', function(e){
+    loginDropdown.on('click', function(e){
         e.stopPropagation();
     })
 
-    $('#roleStudent').on('click', function(e){
-        $('#student-login-collapse').collapse('show');
-        $('#teacher-login-collapse').collapse('hide');
+    loginTabStudent.on('click', function(e){
+        studentLoginCollapse.collapse('show');
+        teacherLoginCollapse.collapse('hide');
+
+        loginTabStudent.addClass('login-tab-selected');
+        loginTabTeacher.removeClass('login-tab-selected');
     })
 
-    $('#roleTeacher').on('click', function(e){
-        $('#student-login-collapse').collapse('hide');
-        $('#teacher-login-collapse').collapse('show');
+    loginTabTeacher.on('click', function(e){
+        studentLoginCollapse.collapse('hide');
+        teacherLoginCollapse.collapse('show');
+
+        loginTabStudent.removeClass('login-tab-selected');
+        loginTabTeacher.addClass('login-tab-selected');
     })
 }
