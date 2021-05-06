7631a8ac3ff5dd0e586ed8624ec4b7944b9fdcc2
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-05-06 08:37:41 -0400

    en cours

--- aquiletour_core/src/main/java/ca/aquiletour/web/pages/git/student_summaries/StudentSummaryViewWeb.java
+++ aquiletour_core/src/main/java/ca/aquiletour/web/pages/git/student_summaries/StudentSummaryViewWeb.java
@@ -1,42 +1,48 @@
 package ca.aquiletour.web.pages.git.student_summaries;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import ca.aquiletour.core.pages.git.student_summaries.StudentSummaryView;
 import ca.aquiletour.core.pages.git.values.StudentSummary;
 import ca.ntro.core.mvc.NtroContext;
 import ca.ntro.core.system.assertions.MustNot;
 import ca.ntro.core.system.trace.T;
+import ca.ntro.models.NtroDate;
 import ca.ntro.web.dom.HtmlElement;
 import ca.ntro.web.mvc.NtroViewWeb;
 
 public class StudentSummaryViewWeb extends NtroViewWeb implements StudentSummaryView {
 
-	@Override
-	public void initializeViewWeb(NtroContext<?, ?> context) {
-		
-	}
+	private HtmlElement studentId;
+	private HtmlElement lastCommitBeforeDealine;
+	private HtmlElement lastCommitAfterDealine;
+	private HtmlElement exerciseCompleted;
+	private HtmlElement exerciseCompletedBeforeDeadline;
 
 	@Override
-	public void displayStudentSummary(StudentSummary studentSummary) {
+	public void initializeViewWeb(NtroContext<?, ?> context) {
 		T.call(this);
-		
-		HtmlElement studentId = this.getRootElement().find("#studentId").get(0);
-		HtmlElement lastCommitBeforeDealine = this.getRootElement().find("#lastCommitBeforeDeadline").get(0);
-		HtmlElement lastCommitAfterDealine = this.getRootElement().find("#lastCommitAfterDeadline").get(0);
-		HtmlElement exerciseCompleted = this.getRootElement().find("#exerciseCompleted").get(0);
-		HtmlElement exerciseCompletedBeforeDeadline = this.getRootElement().find("#exerciseCompletedBeforeDeadline").get(0);
+
+		studentId = this.getRootElement().find("#studentId").get(0);
+		lastCommitBeforeDealine = this.getRootElement().find("#lastCommitBeforeDeadline").get(0);
+		lastCommitAfterDealine = this.getRootElement().find("#lastCommitAfterDeadline").get(0);
+		exerciseCompleted = this.getRootElement().find("#exerciseCompleted").get(0);
+		exerciseCompletedBeforeDeadline = this.getRootElement().find("#exerciseCompletedBeforeDeadline").get(0);
 
 		MustNot.beNull(studentId);
 		MustNot.beNull(lastCommitBeforeDealine);
 		MustNot.beNull(lastCommitAfterDealine);
 		MustNot.beNull(exerciseCompleted);
 		MustNot.beNull(exerciseCompletedBeforeDeadline);
+	}
+
+	@Override
+	public void displayStudentSummary(StudentSummary studentSummary) {
+		T.call(this);
 		
 		studentId.appendHtml(studentSummary.getStudentId());
-		LocalDate dateBefore = Instant.ofEpochMilli(studentSummary.getLastCommitBeforeDeadline()).atZone(ZoneId.systemDefault()).toLocalDate();
-		LocalDate dateAfter = Instant.ofEpochMilli(studentSummary.getLastCommitAfterDeadline()).atZone(ZoneId.systemDefault()).toLocalDate();
+		
+		NtroDate dateBefore = new NtroDate(studentSummary.getLastCommitBeforeDeadline());
+		NtroDate dateAfter = new NtroDate(studentSummary.getLastCommitAfterDeadline());
+
 		lastCommitBeforeDealine.appendHtml(dateBefore.toString());
 		lastCommitAfterDealine.appendHtml(dateAfter.toString());
 		if(studentSummary.isExerciseCompleted()) {
@@ -50,5 +56,4 @@ public class StudentSummaryViewWeb extends NtroViewWeb implements StudentSummary
 			exerciseCompletedBeforeDeadline.appendHtml("Faux");
 		}
 	}
-
 }
