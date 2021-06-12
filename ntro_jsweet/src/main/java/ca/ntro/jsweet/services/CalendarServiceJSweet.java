d3ea8e6e8fcabe75bcc48b5c1c1a948a1a70dc89
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-06-12 07:48:01 -0400

    en cours) formatage date en JSweet

--- ntro_jsweet/src/main/java/ca/ntro/jsweet/services/CalendarServiceJSweet.java
+++ ntro_jsweet/src/main/java/ca/ntro/jsweet/services/CalendarServiceJSweet.java
@@ -1,8 +1,10 @@
 package ca.ntro.jsweet.services;
 
+import ca.ntro.core.system.trace.T;
 import ca.ntro.models.NtroDate;
 import ca.ntro.models.NtroTimeOfDay;
 import ca.ntro.services.CalendarService;
+import def.dom.intl.DateTimeFormatOptions;
 import def.js.Date;
 
 public class CalendarServiceJSweet extends CalendarService {
@@ -14,10 +16,48 @@ public class CalendarServiceJSweet extends CalendarService {
 
 	@Override
 	public String format(long epochSeconds, String dateFormat) {
+		T.call(this);
+
 		Date date = new Date();
 		date.setTime(epochSeconds * 1000);
 		
-		return date.toLocaleTimeString().toString();
+		DateTimeFormatOptions options = new DateTimeFormatOptions() {};
+
+		if(dateFormat.contains("y")) {
+			options.year = "numeric";
+		}
+		
+		if(dateFormat.contains("E")) {
+			options.weekday = "short";
+		}
+
+		if(dateFormat.contains("dd")) {
+
+			options.day = "2-digit";
+
+		}else if(dateFormat.contains("d")) {
+
+			options.day = "numeric";
+		}
+
+		if(dateFormat.contains("M")) {
+			options.month = "short";
+		}
+
+		if(dateFormat.contains("H")) {
+			options.hour12 = false;
+			options.hour = "2-digit";
+		}
+
+		if(dateFormat.contains("m")) {
+			options.minute = "2-digit";
+		}
+
+		if(dateFormat.contains("s")) {
+			options.second = "2-digit";
+		}
+		
+		return date.toLocaleString("fr-CA", options);
 	}
 
 	@Override
