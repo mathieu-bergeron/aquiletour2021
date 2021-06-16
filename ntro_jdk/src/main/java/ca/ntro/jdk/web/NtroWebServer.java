3407dc32107dc5b4ce1765cb471ca84dfed7f410
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-06-16 14:17:55 -0400

    retrait du prototype de reconception

--- ntro_jdk/src/main/java/ca/ntro/jdk/web/NtroWebServer.java
+++ ntro_jdk/src/main/java/ca/ntro/jdk/web/NtroWebServer.java
@@ -1,26 +1,15 @@
 package ca.ntro.jdk.web;
 
-import ca.ntro.NtroBackend;
 import ca.ntro.core.Constants;
 import ca.ntro.core.system.trace.__T;
 import ca.ntro.jdk.services.BackendServiceServer;
-import ca.ntro.jdk.services.EarlyInitializationJdk;
-import ca.ntro.services.ConfigService;
 import ca.ntro.services.EarlyInitialization;
 import ca.ntro.services.MessageService;
 import ca.ntro.services.ModelStore;
-import ca.ntro.services.Ntro;
 import ca.ntro.services.NtroInitializationTask;
 import ca.ntro.services.RouterService;
-import ca.ntro.web.NtroAppWeb;
 
 public class NtroWebServer {
-
-	public static void launchServer(Class<? extends NtroBackend> backendClass, 
-			                           Class<? extends NtroAppWeb> appClass, 
-			                           String[] args) {
-
-	}
 
 	public static NtroInitializationTask defaultInitializationTask(EarlyInitialization earlyInitialization,
 																   Class<? extends BackendServiceServer> backendServiceClass, 
